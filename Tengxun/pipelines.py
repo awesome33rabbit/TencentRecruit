211022275dbd324fecbf55b87d9f16c2ad1d319d
awesome33rabbit <43595445+awesome33rabbit@users.noreply.github.com>
2019-03-27 17:01:58 +0800

    Update pipelines.py

--- Tengxun/pipelines.py
+++ Tengxun/pipelines.py
@@ -16,3 +16,32 @@ class TengxunPipeline(object):
         # print(item['position_address'])
         # print(item['position_date'])
         return item
+
+class MysqlPipeline(object):
+    def __init__(self):
+        self.connect = pymysql.connect(
+            host='localhost',
+            db='scrapy',
+            user='root',
+            passwd='********',
+            port=3306,
+            charset='utf8'
+        )
+        self.cursor = self.connect.cursor()
+
+    def process_item(self, item, spider):
+        try:
+            self.cursor.execute("""
+            insert into tengxun(address, date, link, name, number, type)
+            values (%s, %s, %s, %s, %s, %s)""",
+                                (item['position_address'],
+                                 item['position_date'],
+                                 item['position_link'],
+                                 item['position_name'],
+                                 item['position_number'],
+                                 item['position_type'],
+                                 ))
+            self.connect.commit()
+        except Exception as error:
+            print(error)
+        return item
