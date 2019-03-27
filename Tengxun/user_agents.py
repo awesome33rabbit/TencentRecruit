edb54c75011c651ead48666c8f5720373a3cbb81
awesome33rabbit <43595445+awesome33rabbit@users.noreply.github.com>
2019-03-27 17:00:53 +0800

    Update user_agents.py

--- Tengxun/user_agents.py
+++ Tengxun/user_agents.py
@@ -1,5 +1,6 @@
+from fake_useragent import UserAgent
+
+ua = UserAgent()
 user_agents = [
-        {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36"},
-        {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0"},
-        {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER"},
+        {"User-Agent": ua.random},
 ]
